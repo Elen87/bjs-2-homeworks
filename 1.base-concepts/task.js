5239ee6e2f7ea42377d096e6572028d95337712e
Elen7 <1987tato@rambler.ru>
2025-11-08 16:59:33 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -13,3 +13,17 @@ function solveEquation(a, b, c) {
   const root2 = (-b - Math.sqrt(discriminant)) / ( 2 * a);
   return [root1, root2];
 }
+
+solveEquation(a, b, c);
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let coeff = percent / 100 / 12;
+  let creditBody = amount - contribution;
+  let monthPayment = creditBody * (coeff + (coeff / (((1 + coeff) ** countMonths) - 1)));
+  let total = monthPayment * countMonths;
+  let roundedTotal =  parseFloat(total.toFixed(2));
+
+  return roundedTotal;
+}
+
+calculateTotalMortgage(percent, contribution, amount, countMonths);
